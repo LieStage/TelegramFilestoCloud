c5ff3e9abe8ccca80d74cba6edbb07a434163b69
Wisula <99121477+Wisula@users.noreply.github.com>
2022-08-26 16:24:27 +0530

    Update file_download.py

--- bot/helpers/file_download.py
+++ bot/helpers/file_download.py
@@ -10,11 +10,11 @@ async def download_media(client: CloudBot, message: CallbackQuery, ) -> str:
     user_message = await client.edit_message_text(
         chat_id=message.from_user.id,
         message_id=message.message.message_id,
-        text="processing your request...",
+        text="😍processing your request...",
     )
     try:
         media_id = message.message.reply_to_message
-        await user_message.edit_text("downloading started...")
+        await user_message.edit_text("🙄downloading started...")
         download_file_path = await client.download_media(media_id)
         return download_file_path
     except RPCError as e:
